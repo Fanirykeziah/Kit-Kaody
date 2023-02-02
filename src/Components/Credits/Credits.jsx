f7d9f9e07b8b206f541f0cad54af3d6b46560690
FaniryKeziah <hei.keziah.2@gmail.com>
2023-02-02 12:55:33 +0100

    feat : update post and credits

---
+++ src/Components/Credits/Credits.jsx
@@ -0,0 +1,19 @@
+import '../Credits/credits.css';
+
+export function Credits() {
+    return (
+        <div className="credits">
+          <div className="credits-container">
+            <div className="title"><p>Credits</p></div>
+            <div className="people-credits">
+              <div className="first-credit">
+                <p>Tokimahery Ramarozaka [axios]</p>
+              </div>
+                <div className="second-credit">
+                  <p>Yume Saiko [B.practice] </p>
+                </div>
+              </div>
+          </div>
+        </div>
+    )
+}
---
+++ src/Components/Post/Post.jsx
@@ -0,0 +1,40 @@
+import axios from "axios";
+import '../Post/post.css';
+import { useEffect } from "react";
+import { useState } from "react";
+
+export function Post() {
+    const [feeds , setFeeds] = useState([]);
+
+    useEffect(() => {
+     const feeds = axios.get(" http://localhost:5050/feeds")
+     feeds.then((res) => {
+      setFeeds(res.data);
+      console.log(res);
+    })
+    .catch((err) => {
+    })
+  },[])
+  
+    return (
+        <div className="container-posting">
+        { feeds.map((elt , k ) => (
+          <div className="post-container">
+            <div className="header">
+              <div className="profile">
+                <div className="profile-img"><img src={elt?.user.avatarURL} alt="" className="img"/></div>
+                <div className="profile-name">
+                  <h4>{elt?.user.name}</h4>
+                  <p className="date">{elt?.postingDate}</p>
+                </div>
+              </div>
+            </div>
+            <div className="post">
+              <p className="post-description">{elt?.description}</p>
+              <img src={elt?.imgURL} className="post-img"/>
+            </div>
+          </div>
+          ))}
+        </div>
+    )
+}
